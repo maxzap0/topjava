41032296013d774400ad4ddbfa64c9d4a0736561
xdevil013 <xdevil013@gmail.com>
2022-04-14 15:16:42 +0300

    add csrf

--- src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
@@ -82,7 +82,6 @@ public class AbstractUserServiceTest extends AbstractServiceTest{
     void createWithException() throws Exception {
         validateRootCause(ConstraintViolationException.class, () -> service.create(new User(null, "  ", "mail@yandex.ru", "password", 2000, Role.USER)));
         validateRootCause(ConstraintViolationException.class, () -> service.create(new User(null, "User", "  ", "password", 2000, Role.USER)));
-        validateRootCause(ConstraintViolationException.class, () -> service.create(new User(null, "User", "mail@yandex.ru", "  ", 2000, Role.USER)));
         validateRootCause(ConstraintViolationException.class, () -> service.create(new User(null, "User", "mail@yandex.ru", "password", 9, true, new Date(), Set.of())));
         validateRootCause(ConstraintViolationException.class, () -> service.create(new User(null, "User", "mail@yandex.ru", "password", 10001, true, new Date(), Set.of())));
     }
